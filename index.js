cf10a046369106c9ea3cc675dbba522277d52039
LaraCamarino <laracamarino@gmail.com>
2022-06-19 23:59:07 -0300

    Postar novo tweet

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ app.use(cors());
 app.use(express.json());
 
 const users = [];
+const tweets = [];
 
 app.post("/sign-up", (request, response) => {
 	const { username, avatar } = request.body;
 	if (username.length > 0 && avatar.length > 0) {
 		users.push(request.body);
+		response.send("Ok");
+	}
+	else {
+		response.send("Erro");
+	}
+});
+
+app.post("/tweets", (request, response) => {
+	const { username, tweet } = request.body;
+	if (username && tweet) {
+		tweets.push({
+			username: username,
+			tweet: tweet
+		});
 		response.send("Ok");
 	}
 	else {
